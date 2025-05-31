44a109094c2d69d612087e8be1b55bab68b98392
Harini A <hari628122004@gmail.com>
2025-06-01 00:55:05 +0530

    Create 10 . Display the movies which have not been returned yet.sql

---
+++ 10 . Display the movies which have not been returned yet.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(c.first_name,' ',c.last_name )'CUSTOMER_NAME' ,f.title,r.return_date 
+FROM sakila.rental r
+join inventory i on i.inventory_id=r.inventory_id
+join film f on f.film_id = i.film_id
+join customer c on c.customer_id = r.customer_id
+where r.return_date is null
+group by CUSTOMER_NAME
+order by f.title;
