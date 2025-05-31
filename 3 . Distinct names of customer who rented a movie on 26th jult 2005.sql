38300f64663e3f508566033827f7c660bd27fd9c
Harini A <hari628122004@gmail.com>
2025-06-01 00:34:40 +0530

    Create 3 . Distinct names of customer who rented a movie on 26th jult 2005.sql

---
+++ 3 . Distinct names of customer who rented a movie on 26th jult 2005.sql
@@ -0,0 +1,3 @@
+select distinct r.customer_id,concat(c.first_name , ' ',c.last_name)'Full Name',r.rental_datecustomer from sakila.rental r
+join sakila.customer c on c.customer_id = r.customer_id
+where date(r.rental_date) = '2005-07-26'
